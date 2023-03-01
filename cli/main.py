1203ef1c3ea5881f90e5d7426df1758d6b202ea8
Chris Bacon <chrisbacon2009@hotmail.com>
2023-03-01 14:33:52 +0000

    Refactor

--- cli/main.py
+++ cli/main.py
@@ -48,31 +48,34 @@ def to_lev_or_not_to_lev(
     above_ma_200 = stock_price > ma_200
 
     print(
-        "index_vol_annualised_below_40: ", format_emoji(index_vol_annualised_below_40)
+        f"index_vol_annualised_below_40: {format_emoji(index_vol_annualised_below_40)}",
     )
-    print("index_above_ma_200: ", format_emoji(index_above_ma_200))
-    print("vol_annualised_below_40: ", format_emoji(vol_annualised_below_40))
-    print("above_ma_200: ", format_emoji(above_ma_200))
-
-    return weekly_vol_annualised < 40 and stock_price > ma_200
+    print(
+        f"index_above_ma_200: {format_emoji(index_above_ma_200)}",
+    )
+    print(
+        f"vol_annualised_below_40: {format_emoji(vol_annualised_below_40)}",
+    )
+    print(
+        f"above_ma_200: {format_emoji(above_ma_200)}",
+    )
 
 
 def run(ticker):
-    csvPath = f"./../data/{ticker}-{datetime.today().strftime('%Y-%m-%d')}.csv"
-
     stockDf = None
+    csvPath = f"./../data/{ticker}-{datetime.today().strftime('%Y-%m-%d')}.csv"
 
     try:
         stockDf = pd.read_csv(csvPath)
-        print(f"Reading from CSV {csvPath}\n")
+        print(f"[info] Reading from CSV {csvPath}\n")
     except:
         stockDf = stock_info.get_data(ticker)
         stockDf = stockDf.rename_axis("date").reset_index()
         stockDf.to_csv(csvPath)
-        print(f"Querying Yahoo Finance and saving to {csvPath}\n")
+        print(f"[---] Querying Yahoo Finance and saving to {csvPath}\n")
 
     stock_price = stockDf["adjclose"][-1:].values[0]
-    print("The price as of last close: " + str(stock_price))
+    print(f"[{ticker}] The price as of last close: ${str(round(stock_price, 2))}")
 
     # TODO: Just merge this into stockDf
     dailyChanges = calculate(stockDf, 1, limit)
@@ -90,9 +93,9 @@ def run(ticker):
     num_trading_days = 252
     num_weeks = 52
     num_months = 12
-    daily_vol_annualised = daily_std_dev * math.sqrt(num_trading_days)
-    weekly_vol_annualised = weekly_std_dev * math.sqrt(num_weeks)
-    monthly_vol_annualised = monthly_std_dev * math.sqrt(num_months)
+    daily_vol_annualised = round(daily_std_dev * math.sqrt(num_trading_days), 2)
+    weekly_vol_annualised = round(weekly_std_dev * math.sqrt(num_weeks), 2)
+    monthly_vol_annualised = round(monthly_std_dev * math.sqrt(num_months), 2)
 
     sigma_move = round(
         (largestDailyChange["percent_change"] - average(daily_mean_change))
@@ -100,21 +103,16 @@ def run(ticker):
         2,
     )
 
-    print(sorteddailyChanges)
-
     render(
         ticker=ticker,
-        daily=average(daily_mean_change),
-        weekly=average(weekly_mean_change),
-        monthly=average(monthly_mean_change),
         daily_std_dev=daily_std_dev,
         weekly_std_dev=weekly_std_dev,
         monthly_std_dev=monthly_std_dev,
     )
 
     print(
-        "The largest daily change was "
-        + str(largestDailyChange["percent_change"])
+        "[{ticker}] The largest daily change was "
+        + str(round(largestDailyChange["percent_change"], 2))
         + "% on "
         + str(
             largestDailyChange["date"]
@@ -124,30 +122,31 @@ def run(ticker):
         )
     )
 
-    axarr = dailyChanges.hist(
-        column="percent_change",
-        bins=100,
-        grid=False,
-        figsize=(12, 8),
-        color="#86bf91",
-        zorder=2,
-        rwidth=0.9,
-    )
+    if graph:
+        axarr = dailyChanges.hist(
+            column="percent_change",
+            bins=100,
+            grid=False,
+            figsize=(12, 8),
+            color="#86bf91",
+            zorder=2,
+            rwidth=0.9,
+        )
 
-    for ax in axarr.flatten():
-        ax.set_title("Daily Volatility over the last " + str(limit) + " days")
-        ax.set_xlabel("Daily % Volatility")
-        ax.set_ylabel("Frequency")
+        for ax in axarr.flatten():
+            ax.set_title("Daily Volatility over the last " + str(limit) + " days")
+            ax.set_xlabel("Daily % Volatility")
+            ax.set_ylabel("Frequency")
 
     ma_50 = moving_average(stockDf, 50)["MA 50"]
     ma_200 = moving_average(stockDf, 200)["MA 200"]
 
-    print("\n50 day moving average: " + str(ma_50[-1:].values[0]))
-    print("200 day moving average: " + str(ma_200[-1:].values[0]))
+    print(f"\n[{ticker}] 50 day moving average: {str(round(ma_50[-1:].values[0], 2))}")
+    print(f"[{ticker}] 200 day moving average: {str(round(ma_200[-1:].values[0], 2))}")
 
-    print("\nAnnualised daily volatility: " + str(daily_vol_annualised))
-    print("Annualised weekly volatility: " + str(weekly_vol_annualised))
-    print("Annualised monthly volatility: " + str(monthly_vol_annualised))
+    print(f"\n[{ticker}] Annualised daily volatility: {str(daily_vol_annualised)}%")
+    print(f"[{ticker}] Annualised weekly volatility: {str(weekly_vol_annualised)}%")
+    print(f"[{ticker}] Annualised monthly volatility: {str(monthly_vol_annualised)}%")
 
     return weekly_vol_annualised, stock_price, ma_200[-1:].values[0]
 
--- cli/ui.py
+++ cli/ui.py
@@ -1,11 +1,17 @@
-def render(ticker, daily=0, weekly=0, monthly=0, daily_std_dev=0, weekly_std_dev=0, monthly_std_dev=0):
-    print(f"{ticker} daily mean vol: {daily}%")
-    print(f"{ticker} weekly mean vol: {weekly}%")
-    print(f"{ticker} monthly mean vol: {monthly}%")
-
-    print("\n")
-    print("Standard Deviation")
+def render(
+    ticker,
+    daily_std_dev=0,
+    weekly_std_dev=0,
+    monthly_std_dev=0,
+):
+    print(f"\n{ticker} Standard Deviation")
     print("(68%, 95%, 99.7%)")
-    print(f"Daily 1σ: {daily_std_dev}%, 2σ: {round(daily_std_dev * 2, 2)}%, 3σ: {round(daily_std_dev * 3, 2)}%")
-    print(f"Weekly 1σ: {weekly_std_dev}%, 2σ: {round(weekly_std_dev * 2, 2)}%, 3σ: {round(weekly_std_dev * 3, 2)}%")
-    print(f"Monthly 1σ: {monthly_std_dev}%, 2σ: {round(monthly_std_dev * 2, 2)}%, 3σ: {round(monthly_std_dev * 3, 2)}%")
+    print(
+        f"[{ticker}] Daily 1σ: {daily_std_dev}%, 2σ: {round(daily_std_dev * 2, 2)}%, 3σ: {round(daily_std_dev * 3, 2)}%"
+    )
+    print(
+        f"[{ticker}] Weekly 1σ: {weekly_std_dev}%, 2σ: {round(weekly_std_dev * 2, 2)}%, 3σ: {round(weekly_std_dev * 3, 2)}%"
+    )
+    print(
+        f"[{ticker}] Monthly 1σ: {monthly_std_dev}%, 2σ: {round(monthly_std_dev * 2, 2)}%, 3σ: {round(monthly_std_dev * 3, 2)}%"
+    )
